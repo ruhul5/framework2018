41179e4d4548f337646beb65b889f0deb9c8c027
Ruhul Alam <ruhul51218@gmail.com>
2018-04-08 12:02:58 -0400

    my first commit

---
+++ src/tests/myfirsttest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class myfirsttest {
+
+	@Test 
+	public void testingFirstName() {
+
+
+
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+		System.setProperty("webdriver.chrome.driver","//Users//ruhulalam//framework2018//drivers//chromedriver");
+		driver.get ("http://www.facebook.com");
+
+
+	}
+}	//
